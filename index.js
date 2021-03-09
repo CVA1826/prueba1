506265b6fe25f1200bd0ed14fd586e568cb5b2b8
CVA1826 <79380824+CVA1826@users.noreply.github.com>
2021-03-09 15:35:43 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("Hola mundo") //string
+console.log("Adios mundo")
+console.log(221826) //int
+console.log(true) //bool
+
+let color = "Azul" //declaración+asignación
+
+console.log(color)
+
+color = 123
+
+console.log(color)
+
+const color2 = "Negro"
+
+console.log(color2)
+
+let ciudad = "Londres"
+console.log(ciudad)
+ciudad = null
+console.log(ciudad)
+
+alert("Solo estoy probando esto")
+square("red")
